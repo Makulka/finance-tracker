5797f5564e0bc32a3b48456e0a344614b9beec7f
Stania Svobodova <greg.morris85@gmail.com>
2018-12-17 17:47:08 +0000

    final commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ApplicationRecord
   has_many :user_stocks
   has_many :stocks, through: :user_stocks
   
+  has_many :friendships
+  has_many :friends, through: :friendships
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
@@ -26,6 +29,46 @@ class User < ApplicationRecord
   def can_add_stock?(ticker_symbol)
     under_stock_limit? && !stock_already_added?(ticker_symbol)
   end
+  
+  def self.search(param)
+    param.strip!
+    param.downcase!
+    to_send_back = (first_name_matches(param) + last_name_matches(param) + email_matches(param)).uniq
+    return nil unless to_send_back
+    to_send_back
+  end
+  
+  def self.first_name_matches(param)
+    matches('first_name', param)
+  end
+  
+  def self.last_name_matches(param)
+    matches('last_name', param)
+  end
+  
+  def self.email_matches(param)
+    matches('email', param)
+  end
+  
+  def self.matches(field_name, param)
+    where("#{field_name} like ?", "%#{param}%")
+  end
+  
+  def except_current_user(users)
+    users.reject { |user| user.id == self.id }
+  end
+  
+  def not_friends_with?(friend_id)
+    friendships.where(friend_id: friend_id).count < 1
+  end
+  
+  # def self.search(param, ignore_user)
+  #   param.strip.downcase!
+  #   User.where('first_name LIKE ?', "%#{param}%")
+  #       .or(User.where('last_name LIKE ?', "%#{param}%"))
+  #       .or(User.where('email LIKE ?', "%#{param}%"))
+  #       .where.not(id: ignore_user.id)
+  # end
   
   
   
